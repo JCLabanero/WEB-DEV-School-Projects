0505fef031cdbfab0ef8ddd51067499a02477e77
unknown <johncarlolabanero@gmail.com>
2022-11-18 12:39:04 +0800

    remove array in js

--- unit 3 lesson 5 try this on your own/unit 3 lesson 5 try this on your own.js
+++ unit 3 lesson 5 try this on your own/unit 3 lesson 5 try this on your own.js
@@ -1,45 +1,44 @@
-var thisIsArray = ["12046,Hanie,Gabay,BSIT","12050,Karla,Primo,BSM","12051,Gelanne,Teodoro,BIT/CT","12052,Eris,Ocado,BEED","12057,Albert,Reyes,BSIT","12060,Ruben,Ramos,BIT/FT"];
+// var thisIsArray = ["12046,Hanie,Gabay,BSIT","12050,Karla,Primo,BSM","12051,Gelanne,Teodoro,BIT/CT","12052,Eris,Ocado,BEED","12057,Albert,Reyes,BSIT","12060,Ruben,Ramos,BIT/FT"];
 
 
-function searchID(){
-    var idNumber = prompt("Enter the ID that will be edited","12345");
-    var cntr=0;
-    while(cntr<thisIsArray.length){
-        var arr = thisIsArray[cntr].split(",");
-        // document.write(cntr+" "+arr[0]+"<br>");
-        if(arr[0]==idNumber)
-            return cntr;
-        cntr++;
-    }
-    alert("ID doesn't exist");
-    return -1;
-}
-function editData(){
-    var index = searchID();
-    if(index<0)
-        return false;
-    var selectToChange = prompt("Choose what to edit:\n[1]ID\n[2]First Name\n[3]Last Name\n[4]Course");
+// function searchID(){
+//     var idNumber = prompt("Enter the ID that will be edited","12345");
+//     var cntr=0;
+//     while(cntr<thisIsArray.length){
+//         var arr = thisIsArray[cntr].split(",");
+//         // document.write(cntr+" "+arr[0]+"<br>");
+//         if(arr[0]==idNumber)
+//             return cntr;
+//         cntr++;
+//     }
+//     alert("ID doesn't exist");
+//     return -1;
+// }
+// function editData(){
+//     var index = searchID();
+//     if(index<0)
+//         return false;
+//     var selectToChange = prompt("Choose what to edit:\n[1]ID\n[2]First Name\n[3]Last Name\n[4]Course");
 
     
-    var toChange = prompt("");
-}
+//     var toChange = prompt("");
+// }
 
-function closeAndReturn(){
-    let table = document.getElementById("table");
-    for(var a=0; a<thisIsArray.length;a++){
-        var cut = thisIsArray[a].split(",");
-        insertData(cut[0],cut[1],cut[2],cut[3]);
-        // var row = table.insertRow();
-        // var cell0 = row.insertCell(0);
-        // var cell1 = row.insertCell(1);
-        // var cell2 = row.insertCell(2);
-        // var cell3 = row.insertCell(3);
-        // cell0.innerHTML = cut[0];
-        // cell1.innerHTML = cut[1];
-        // cell2.innerHTML = cut[2];
-        // cell3.innerHTML = cut[3];
-    }
-}
+// function closeAndReturn(){
+//     for(var a=0; a<thisIsArray.length;a++){
+//         var cut = thisIsArray[a].split(",");
+//         insertData(cut[0],cut[1],cut[2],cut[3]);
+//         // var row = table.insertRow();
+//         // var cell0 = row.insertCell(0);
+//         // var cell1 = row.insertCell(1);
+//         // var cell2 = row.insertCell(2);
+//         // var cell3 = row.insertCell(3);
+//         // cell0.innerHTML = cut[0];
+//         // cell1.innerHTML = cut[1];
+//         // cell2.innerHTML = cut[2];
+//         // cell3.innerHTML = cut[3];
+//     }
+// }
 //
 
 function addFunction(){
@@ -47,7 +46,11 @@ function addFunction(){
     var inputFName = addForm.i02.value;
     var inputLName = addForm.i03.value;
     var inputCourse = addForm.i04.value;
+    var hold;
     thisIsArray.push(inputID+","+inputFName+","+inputLName+","+inputCourse);
+    for(var a=0; a<thisIsArray.length; a++){
+        hold = thisIsArray[a]+"\n";
+    }
     insertData(inputID,inputFName,inputLName,inputCourse);
     // var inputID = prompt("Input ID number: ", "12345");
     // var inputFName = prompt("Input First Name: ", "John");
@@ -60,6 +63,7 @@ function addFunction(){
     // }
 }
 function insertData(id,fname,lname,course){
+    let table = document.getElementById("table");
     var row = table.insertRow();
     var cell0 = row.insertCell(0);
     var cell1 = row.insertCell(1);
