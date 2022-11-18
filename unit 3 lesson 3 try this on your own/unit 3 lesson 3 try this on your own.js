12688b54cf8ac9975ee3a78ffc4ea0545cdc2ab4
unknown <johncarlolabanero@gmail.com>
2022-11-19 01:01:38 +0800

    lesson 3 Final

--- unit 3 lesson 3 try this on your own/unit 3 lesson 3 try this on your own.js
+++ unit 3 lesson 3 try this on your own/unit 3 lesson 3 try this on your own.js
@@ -1,6 +1,9 @@
-var fnumber=39;
-var snumber=50;
-var tnumber=11;
+var inputfnumber=prompt("Enter first number: ", "39");
+var inputsnumber=prompt("Enter second number: ", "50");
+var inputtnumber=prompt("Enter second number: ", "11");
+var fnumber=parseInt(inputfnumber);
+var snumber=parseInt(inputsnumber);
+var tnumber=parseInt(inputtnumber);
 
 var temp = [fnumber, snumber,tnumber];
 
@@ -10,21 +13,29 @@ for(a=0; a<3; a++){
     }
 }
 
-
-// document.write(temp[0]+" "+temp[1]+" "+temp[2])+"<br>";
-
-if(temp[0]==temp[1]){
-    document.write("true");
-} else
-if(temp[0]==temp[2]){
-    document.write("true");
-} else 
-if(temp[2]==temp[3]){
-    document.write("true");
-} else {
-    document.write("false");
+var check = confirm("Are you sure?");
+if(check==true){
+    changeValue();
 }
+
 function changeValue(){
-    var fnumberHTML = document.querySelector("#f1");
-    fnumberHTML.innerHTML+=fnumber+"";
+    var fnumberHTML = document.getElementById("f1");
+    var snumberHTML = document.getElementById("s2");
+    var tnumberHTML = document.getElementById("t3");
+    var outputHTML = document.getElementById("o4");
+    fnumberHTML.innerHTML = fnumber;
+    snumberHTML.innerHTML = snumber;
+    tnumberHTML.innerHTML = tnumber;
+
+    if(temp[0]==temp[1]){
+        outputHTML.innerHTML = "true";
+    } else
+    if(temp[0]==temp[2]){
+        outputHTML.innerHTML = "true";
+    } else 
+    if(temp[2]==temp[3]){
+        outputHTML.innerHTML = "true";
+    } else {
+        outputHTML.innerHTML = "false";
+    }
 }
